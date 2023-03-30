8a450a0a1cddbc0df0355ea1688626dedc631462
dhseo <dhseo@coretrust.com>
2023-03-30 13:47:51 +0900

    lalala

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-// For command line git tutorial
-// 1st : Local repository modification
-// 2nd : GitHub modification
+#For command line git tutorial
+#1st : Local repository modification
+#2nd : GitHub modification
 
 
 print("Hello world")
